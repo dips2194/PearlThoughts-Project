c82d1fae73e826d26bc1fbb88289c8aac213faed
Deepak Indrale <128774948+dips2194@users.noreply.github.com>
2023-03-27 11:17:32 +0530

    Update backend.tf

--- devops1/backend.tf
+++ devops1/backend.tf
@@ -1,11 +1,11 @@
 
-#terraform {
-#  backend "s3" {
-#    bucket = "mukhed-1001"
-#    key    = "dips.tfstate"
-#    region = "us-east-1"
-#    encrypt = true
-#    dynamodb_table = "terraform-state-lock-dynamo"
-#    }
-#  }
+terraform {
+  backend "s3" {
+    bucket = "mukhed-1001"
+    key    = "dips.tfstate"
+    region = "us-east-1"
+    encrypt = false
+    dynamodb_table = "terraform-state-lock-dynamo"
+    }
+  }
 
